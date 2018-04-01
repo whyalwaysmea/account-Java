27cb6974f726863d7b556348ce54db46e19c5b12
404402223@qq.com <404402223@qq.com>
2018-04-01 11:30:20 +0800

    init project

---
+++ src/main/java/com/whyalwaysmea/account/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.whyalwaysmea.account.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @Author: HanLong
+ * @Date: Create in 2018/4/1 11:09
+ * @Description:    Swagger的配置文件
+ */
+@Profile("dev")
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.whyalwaysmea.account"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("记账")
+                .termsOfServiceUrl("http://www.whyalwaysmea.cn")
+                .description("详情项目信息请关注：http://www.whyalwaysmea.cn")
+                .version("1.0")
+                .build();
+    }
+}
